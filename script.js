364f4260f25e93b1418991ec2868ed9fdab1636f
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-12-02 21:12:05 +0900

    typeDisplayの変数を取得する

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const RANDOM_SENTENCE_URL_API = "https://api.quotable.io/random";
+const typeDisplay = document.getElementById("typeDisplay");
 
 /* 非同期でランダムな文章を取得する */
 function GetRandomSentence() {
