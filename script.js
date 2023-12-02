1a85628628aeaa27b2b9d295e6766a79ac312d36
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-12-02 20:54:56 +0900

    consoleを使ってjsの中身を確認する

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ const RANDOM_SENTENCE_URL_API = "https://api.quotable.io/random";
 function GetRandomSentence() {
     return fetch(RANDOM_SENTENCE_URL_API)
     .then((response) => response.json())
-    .then((data) => data.content);
+    .then((data) => console.log(data.content));
 }
