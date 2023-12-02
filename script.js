99cfc1024d193a1c7c530d01e2453d9d93b2a7fb
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-12-02 20:53:54 +0900

    processを記述する

--- script.js
+++ script.js
@@ -2,5 +2,7 @@ const RANDOM_SENTENCE_URL_API = "https://api.quotable.io/random";
 
 /* 非同期でランダムな文章を取得する */
 function GetRandomSentence() {
-    return fetch(RANDOM_SENTENCE_URL_API);
+    return fetch(RANDOM_SENTENCE_URL_API)
+    .then((response) => response.json())
+    .then((data) => data.content);
 }
