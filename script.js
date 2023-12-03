9331c899bd9501c5404da70ddbf23eada75e3b0a
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-12-03 13:51:12 +0900

    typeInputの変数を宣言する

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const RANDOM_SENTENCE_URL_API = "https://api.quotable.io/random";
 const typeDisplay = document.getElementById("typeDisplay");
+const typeInput = document.getElementById("typeInput");
 
 /* 非同期でランダムな文章を取得する */
 function GetRandomSentence() {
@@ -23,6 +24,8 @@ async function RenderNextSentence() {
         typeDisplay.appendChild(characterSpan);
         characterSpan.classList.add("correct");
     });
+
+    /* テキストボックスの中身を消す */
 }
 
 RenderNextSentence();
