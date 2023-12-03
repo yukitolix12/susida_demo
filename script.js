ea8c9bc6c0b96fa2fd10e67f140705df5fb2aca9
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-12-03 13:56:11 +0900

    typingの正誤判定機能作成

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const RANDOM_SENTENCE_URL_API = "https://api.quotable.io/random";
 const typeDisplay = document.getElementById("typeDisplay");
 const typeInput = document.getElementById("typeInput");
 
+/*  inputテキスト入力。合っているかどうかの判定。*/
+typeInput.addEventListener("input", () => {
+
+});
+
 /* 非同期でランダムな文章を取得する */
 function GetRandomSentence() {
     return fetch(RANDOM_SENTENCE_URL_API)
