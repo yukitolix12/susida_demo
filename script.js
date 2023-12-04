2575e29ac1654a935b2a0446e9cb3c02c01272e5
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-12-05 08:05:42 +0900

    consoleをコメントアウト

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ let originTime = 30;
 function StartTimer() {
     timer.innerText = originTime;
     startTime = new Date();
-    console.log(startTime);
+    // console.log(startTime);
 }
 
 RenderNextSentence();
