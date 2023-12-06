6e371f92d3c4b3647a2e34878b8ab356a5235a3a
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-12-06 22:04:54 +0900

    originTimeを60秒に再設定し、試し打ちでcorrect音を確認する

--- script.js
+++ script.js
@@ -74,7 +74,7 @@ async function RenderNextSentence() {
 }
 
 let startTime;
-let originTime = 30;
+let originTime = 60;
 function StartTimer() {
     timer.innerText = originTime;
     startTime = new Date();
