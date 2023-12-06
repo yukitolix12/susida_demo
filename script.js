bb6f70660b304b2f3f826747f85e7e06f71b7c5a
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-12-06 21:30:36 +0900

    originTimeを50に設定する

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ async function RenderNextSentence() {
 }
 
 let startTime;
-let originTime = 30;
+let originTime = 50;
 function StartTimer() {
     timer.innerText = originTime;
     startTime = new Date();
